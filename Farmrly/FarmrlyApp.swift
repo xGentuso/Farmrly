c89294f451b6b4033067797cd48c09c12d8b2a42
xGentuso <ryan.mota@triosstudent.com>
2025-01-23 10:53:28 -0500

    Initial Commit

---
+++ Farmrly/FarmrlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmrlyApp.swift
+//  Farmrly
+//
+//  Created by ryan mota on 2025-01-23.
+//
+
+import SwiftUI
+
+@main
+struct FarmrlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
